ceb5afdc19c2a1d59155d68267ec7763fa85ed21
Adam Crosby <adam@uptill3.com>
2012-08-31 02:49:42 -0400

    Initial Application

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ elastic-flask-baseline
 ======================
 
 A baseline Flask application suitable for use on Amazon Elastic Beanstalk.
+
+The code here is kept as minimal as possible, while still demonstrating the critical points/differences that running on Elastic Beanstalk requires.
---
+++ application.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from flask import *
+
+# The WSGI configuration on Elastic Beanstalk requires 
+# the callable be named 'application' by default.
+application = Flask(__name__)
+
+# Setup the root route of the website, and render the 'index.html' template
+@application.route("/")
+def default():
+	#display welcome page
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	application.debug = True
+	application.run(host='0.0.0.0')
